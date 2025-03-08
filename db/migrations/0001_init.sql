4f368c0d3de8a7bed7a98065dfb92ecfdeb09562
7lawa9111 <mohamedawad_1@hotmail.com>
2025-03-09 00:24:57 +0400

    OPEA - Docker compose

--- db/migrations/0001_init.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE study_sessions (
-    id INTEGER PRIMARY KEY,
-    group_id INTEGER,
-    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (group_id) REFERENCES groups(id)
-); 
